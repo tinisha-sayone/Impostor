ba0bdd70dfe2490c85a72a7ac55df399ea1fb6de
tinisha <you@example.com>
2020-01-23 18:43:08 +0530

    import changes made

--- impostor/forms.py
+++ impostor/forms.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
-class BigAuthenticationForm(AuthenticationForm):
-	username = forms.CharField(label=_("Username"), max_length=70)
 
+class BigAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label=_("Username"), max_length=70)
